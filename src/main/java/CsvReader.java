f909aabf70b2f0f66aeee8b8a3332e15a6f41132
Kemal Toprak Uçar <toprakucar@gmail.com>
2017-05-26 10:17:25 +0300

    add: test_read_file

--- src/main/java/CsvReader.java
+++ src/main/java/CsvReader.java
@@ -1,25 +1,20 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by toprak.ucar on 24/05/2017.
  */
 public class CsvReader {
-    private String csvFile;
-    private String line = "";
-    private BufferedReader br;
-    private List<String> rows = new ArrayList<String>();
 
     public List<String> Reader(String fileName){
+        String line;
+        BufferedReader br = readFile(fileName);
+        List<String> rows = new ArrayList<String>();
+
         try {
-            br = new BufferedReader(new FileReader(csvFile));
             while ((line = br.readLine()) != null) {
-                rows.add(line);
+                rows.add(line.trim());
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -27,6 +22,18 @@ public class CsvReader {
             e.printStackTrace();
         }
         return rows;
+    }
+
+    private BufferedReader readFile(String fileName)  {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
+        BufferedReader bf = null;
+        try {
+            bf = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return bf;
     }
 
 }
--- src/test/java/TestReader.java
+++ src/test/java/TestReader.java
@@ -1,5 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * Created by toprak.ucar on 24/05/2017.
  */
 public class TestReader {
+    CsvReader reader;
+    String fileName;
+    List<String> linesCovered;
+
+
+    @Before
+    public void setUp(){
+        reader = new CsvReader();
+        fileName = "test.csv";
+        linesCovered = new ArrayList<String>();
+    }
+
+    @Test
+    public void test_read_file(){
+        linesCovered = reader.Reader(fileName);
+        assertEquals(3, linesCovered.size());
+        //assertEquals("david;beckham;7", linesCovered.get(0));
+        assertEquals("michael;owen;10", linesCovered.get(1));
+        assertEquals("steven;gerrard;8", linesCovered.get(2));
+
+    }
+
+
+
 }
